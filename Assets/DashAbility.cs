577121712b257750d1eaf8bc50cd66b795190ca2
Daniel-something <79511435+Daniel-something@users.noreply.github.com>
2021-03-10 14:42:02 +0100

    Början på Dash ability
    Tryck på enter för att dasha

---
+++ Assets/DashAbility.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashAbility : MonoBehaviour
+{
+    public float DashSpeed = 500;
+    private Rigidbody2D rb;
+    public bool hasDashed = false;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Dash") && !hasDashed)
+        {
+            print("Dash");
+            hasDashed = true;
+            rb.AddForce(new Vector2(DashSpeed, 0));
+            rb.velocity = new Vector3(rb.velocity.x, 0);
+        }
+    }
+
+
+    private void StopDash()
+    {
+        //Invoke StopDash, få den att vänta tills dashen ska sluta och stoppa den efter det
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Ground")
+        {
+            hasDashed = false;
+        }
+    }
+}
+
