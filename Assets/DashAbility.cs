19d9a8e4dbde0d8e6169a45d6b5a292f04d04994
Daniel-something <79511435+Daniel-something@users.noreply.github.com>
2021-03-10 15:23:56 +0100

    Uppdatering till Dash
    Gjorde så att man kan dasha åt båda hållen

--- Assets/DashAbility.cs
+++ Assets/DashAbility.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DashAbility : MonoBehaviour
 {
-    public float DashSpeed = 500;
+    public float DashSpeed = 100;
     private Rigidbody2D rb;
     public bool hasDashed = false;
 
@@ -17,26 +17,26 @@ public class DashAbility : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Dash") && !hasDashed)
+        if (Input.GetButtonDown("Dash") && !hasDashed) //Kollar om man trycker på Dashknapppen och om hasDashed är false. - Daniel
         {
             print("Dash");
-            hasDashed = true;
-            rb.AddForce(new Vector2(DashSpeed, 0));
-            rb.velocity = new Vector3(rb.velocity.x, 0);
+            hasDashed = true; //Ställer om hasDashed till true när man dashar så att man inte kan göra det oändligt. - Daniel
+            rb.AddForce(new Vector2(Input.GetAxis("Horizontal") * DashSpeed, 0)); //Multiplicerar karaktärens horizontal speed med Dashspeed och därmed gör så att man dashar åt det hållet. - Daniel
+            rb.velocity = new Vector3(rb.velocity.x, 0); //Fryser karaktärens Y-position så att man inte ska falla neråt under en dash, tyvärr fryser den endast positionen under en frame. - Daniel
         }
     }
 
 
     private void StopDash()
     {
-        //Invoke StopDash, få den att vänta tills dashen ska sluta och stoppa den efter det
+        //Invoke StopDash, få den att vänta tills dashen ska sluta och stoppa den efter det (Saker jag (Daniel) ska lägga till senare) - Daniel
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.tag == "Ground")
         {
-            hasDashed = false;
+            hasDashed = false; //Ställer tillbaka hasDashed till true när man nuddar marken så att man kan dasha igen. - Daniel
         }
     }
 }
